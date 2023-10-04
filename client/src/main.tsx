7c58d06017e48ddf3c3c46bf7b98be6fe02e5807
Giga Amiridze <gigaamiridze@gmail.com>
2023-10-04 12:47:15 +0400

    [Client] Added declaration file and provider for theme

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
