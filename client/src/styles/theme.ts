0ce8c423f036d8708b9ff85fad670bc2cd5d059e
Giga Amiridze <gigaamiridze@gmail.com>
2023-10-05 13:09:36 +0400

    [Client] Added theme colors

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -2,6 +2,16 @@ export const theme = {
   colors: {
     white: '#FFFFFF',
     black: '#000000',
+    primary: '#1a1a1a',
+    line: 'rgba(0, 0, 0, .125)',
+    yellow: '#f8e825',
+    cultured: '#f7f7f9',
+    gray: 'hsla(0, 0%, 100%, .5)',
+    brightGray: '#ececec',
+    davyGray: '#55595c',
+    darkGray: '#343a40',
+    lightGreen: '#4bbf73',
+    darkGreen: 'rgba(75, 191, 115, .5)',
   },
   fonts: {
     primary: '"Nunito Sans", sans-serif',
