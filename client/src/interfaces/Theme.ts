df4f2ee3b6826d73b3035f8c41274fb5ef914c1b
Giga Amiridze <gigaamiridze@gmail.com>
2023-10-05 13:09:36 +0400

    [Client] Added theme colors

--- client/src/interfaces/Theme.ts
+++ client/src/interfaces/Theme.ts
@@ -6,6 +6,16 @@ export interface ITheme {
 interface IColors {
   white: string;
   black: string;
+  primary: string;
+  line: string;
+  yellow: string;
+  cultured: string;
+  gray: string;
+  brightGray: string;
+  davyGray: string;
+  darkGray: string;
+  lightGreen: string;
+  darkGreen: string;
 }
 
 interface IFonts {
